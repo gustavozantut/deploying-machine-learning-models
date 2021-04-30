b83acb90ac01393a416e796077bd39936c175136
gustavozantut <gustavo_zantut@hotmail.com>
2021-04-30 01:03:45 -0300

    now?

--- scripts/fetch_kaggle_large_dataset.sh
+++ scripts/fetch_kaggle_large_dataset.sh
@@ -6,4 +6,4 @@ NOW=$(date)
 kaggle datasets download -d $TRAINING_DATA_URL -p packages/neural_network_model/neural_network_model/datasets/ && \
 unzip packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset.zip -d packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset && \
 echo $TRAINING_DATA_URL 'retrieved on:' $NOW > packages/neural_network_model/neural_network_model/datasets/training_data_reference.txt && \
-rename "s/\’//g" *
+rename "s/\���//g" *
