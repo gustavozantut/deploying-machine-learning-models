50de4a93dd9fe6daebdcef303e59a5ec4120f9a4
gustavozantut <gustavo_zantut@hotmail.com>
2021-04-30 01:25:46 -0300

    now?

--- Dockerfile
+++ Dockerfile
@@ -12,7 +12,6 @@ ENV FLASK_APP run.py
 ADD ./packages/ml_api /opt/ml_api/
 RUN pip install --upgrade pip
 RUN pip install -r /opt/ml_api/requirements.txt
-RUN apt-get install rename
 
 RUN chmod +x /opt/ml_api/run.sh
 RUN chown -R ml-api-user:ml-api-user ./
--- scripts/fetch_kaggle_large_dataset.sh
+++ scripts/fetch_kaggle_large_dataset.sh
@@ -6,4 +6,6 @@ NOW=$(date)
 kaggle datasets download -d $TRAINING_DATA_URL -p packages/neural_network_model/neural_network_model/datasets/ && \
 unzip packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset.zip -d packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset && \
 echo $TRAINING_DATA_URL 'retrieved on:' $NOW > packages/neural_network_model/neural_network_model/datasets/training_data_reference.txt && \
-rename "s/\���//g" *
+cd packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset && \
+mv *Shepherd* Shepherds Purse
+
